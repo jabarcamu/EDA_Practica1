f71c7f4467f7d4d5adce6ad9debd8335489d1adb
chiconasa3000 <csucavelando@gmail.com>
2021-06-21 10:21:54 -0500

    upload gitignore

--- 2_implementacion_algoritmos/algorithms/main.py
+++ 2_implementacion_algoritmos/algorithms/main.py
@@ -1,9 +1,12 @@
 from heapsort import HeapSort
 from quicksort import QuickSort
+import time
+import glob
 
 heap_sort = HeapSort()
 quick_sort = QuickSort()
 
+print(glob.glob)
 
 #Filling the array with testData
 arr = []
@@ -16,9 +19,13 @@ sortSelected = input("Ingrese Tipo Ordenamiento:")
 
 if (sortSelected == "0"):
     #Apply Heapsort algorithm
+    startTime = time.time()
     heap_sort.heapsort(arr)
+    print("heapsort take %s seconds"%(time.time() - startTime))
     print(arr)
 elif (sortSelected == "1"):
     #Apply QuickSort algorithm
+    startTime = time.time()
     quick_sort.quicksort(0,len(arr)-1,arr)
+    print("heapsort take %s seconds"%(time.time() - startTime))
     print(arr)
