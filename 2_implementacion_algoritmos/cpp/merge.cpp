8f1a76045212d1b5fca7afee3d49141eb72149e2
jabarcamu <jabarcamu@unsa.edu.pe>
2021-06-25 21:46:31 -0500

    actualizando main desde branch-arturo, algoritmos counting sort y merge sort

---
+++ 2_implementacion_algoritmos/cpp/merge.cpp
@@ -0,0 +1,158 @@
+
+// Fuente : https://www.geeksforgeeks.org/merge-sort/
+// This code is contributed by Mayank Tyagi
+
+// Algoritmo de ordenamiento MERGE SORT - divide y conquistarás
+
+#include <bits/stdc++.h>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// arr, arreglo de números
+// l, índice del límite inferior del arreglo
+// m, índice medio entre el límite inferior l y superior r, del arreglo
+// r, índice del límite superior del arreglo
+void merge(int arr[], int l, int m, int r)
+{
+	int n1 = m - l + 1; // cantidad de elementos del arreglo izquierdo.
+	int n2 = r - m; // cantidad de elementos del arreglo derecho.
+
+	// creación de los arreglos temporales izquierdo y derecho respectivamente.
+	int L[n1], R[n2];
+
+	// copia de los valores en el arreglo izquierdo(arreglo esta ordenado)
+	for (int i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	// copia de los valores en el arreglo derecho(arreglo esta ordenado)
+	for (int j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];	
+
+	// indice para el arreglo izquierdo
+	int i = 0;
+
+	// indice para el arreglo derecho
+	int j = 0;
+
+	// indice para el arreglo ordenado final
+	int k = l;
+
+	// copiar elementos en el arreglo principal, con elementos del arreglo izquierdo y/o derecho.
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	// copiar elementos en el arreglo principal, con elementos del arreglo izquierdo(restantes)
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	// copiar elementos en el arreglo principal, con elementos del arreglo derecho(restantes)
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+
+// arr, arreglo no ordenado de números.
+// l, índice inferior del arreglo
+// r, índice superior del arreglo
+void mergeSort(int arr[],int l,int r){
+	if(l>=r){
+		return; // retorno de la recursividad(paso base)
+	}
+	int m =l+ (r-l)/2; // calculando el índice del punto medio del arreglo
+	mergeSort(arr,l,m); // llamada recursiva del arreglo izquierdo
+	mergeSort(arr,m+1,r); // llamada recursiva del arreglo derecho
+	merge(arr,l,m,r); // combinar arreglo izquierdo y derecho
+}
+
+// imprimir arreglo
+void printArray(int A[], int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << A[i] << " ";			
+}
+
+int main()
+{
+	
+
+	// generar los n valores de numeros(100, 500, ..., 100000)
+    vector<int> generate; 
+    generate.push_back(100);
+    generate.push_back(500);
+
+    for(int i=1000 ; i < 10000 ; i+=1000) {
+        generate.push_back(i);
+    }
+    for(int i=10000 ; i < 100001 ; i+=10000) {
+        generate.push_back(i);
+    }
+
+
+    // archivo para alamacenar los tiempos de ejecución
+    ofstream myfile;
+    myfile.open ("mergeSort_cpp_time.txt");
+
+    for (int k = 0; k < generate.size(); k++)
+    {
+        // carga de números alearios generados para cada caso de prueba       
+        ifstream infile2("../../1_preparacion_datos/generatedTestData/example_" + to_string(generate[k]) + ".txt");
+        
+        int arr[generate[k]]; // creando el arreglo para los n valores de números
+        int i=0;
+        string line;
+        while (getline(infile2, line))
+        {
+            istringstream iss(line);
+            int a;
+            if (!(iss >> a)) { break; }			
+            arr[i] = a;
+            i++;
+        }
+		
+        // solo para tener el numero de array, igualmente el valor ya es conocido
+        int n = sizeof(arr) / sizeof(arr[0]);		     
+			  
+        // calculando el tiempo de ejecución del algoritmo.
+        clock_t start, end;
+        start = clock();    
+        mergeSort(arr, 0, n - 1);
+        end = clock();
+		
+        // tiempo de ejecucion obtenido
+        double time_taken = double(end - start) / double(CLOCKS_PER_SEC); 
+    
+        cout << "Se ordeno el array de tamanho " + to_string(n) + " :";    
+        cout << "\n Tiempo tomado por el programa es: " << fixed
+             << time_taken << setprecision(8);
+        cout << " segundos " << endl;
+
+        // guardando resultados de ejecución del algoritmo para cada caso de prueba
+        myfile << generate[k] << " "; 
+        myfile << fixed << time_taken;    
+        myfile << "\n";
+    }
+
+    myfile.close();
+
+}
+
+
--- 2_implementacion_algoritmos/readme.md
+++ 2_implementacion_algoritmos/readme.md
@@ -30,3 +30,71 @@ Bubble Sort:
 	- Copiar el codigo "codigo_bubble_sort.java" (ponerlo como ejecucion principal)
 	- En la misma carpeta del proyecto ingresar los .txt a ordenar
 	- La salida se mostrará en consola del Java
+
+
+
+# Algoritmo de ordenamiento - Counting Sort
+```
+counting-sort(A,B,k)
+    for i <- 0 to k
+        do C[i] <- 0
+
+    for j <- 1 to length[A]
+        do C[A[j]] <- C[A[j]] + 1
+    
+    for i <- 1 to k
+        do C[i] <- C[i] + C[i - 1]
+    
+    for j <- length[A] to downto 1
+        do B[C[A[j]]] <- A[j]
+           C[A[j]] <- C[A[j]] - 1 
+```
+
+# Algoritmo de ordenamiento - Merge Sort
+```
+MergeSort(A,l,r)
+    If r > l
+        1. Encontrar el punto medio para dividir el arreglo en dos mitades
+            middle m = l + (r - l)/2
+        2. Llamar a mergeSort para la mitad izquierda
+            call mergeSort(A,l,m)
+        3. Llamar a mergeSort para la mitad derecha
+            call mergeSort(A,m+1,r)
+        4. Combinar las mitades ordenadas de los pasos 2 y 3
+            call merge(A,l,m,r)
+merge(A,l,m,r)
+
+    n1 <- m - l + 1; 
+	n2 <- r - m; 
+
+	L[n1], R[n2];
+	
+    for i <- 0 to n1
+        do L[i] <- A[l+i]
+
+    for j <- 0 to n2
+        do R[j] <- A[m+i+j]
+
+	i<-0
+    j<-0
+    k<-l
+		
+	while i < n1 AND j < n2 do
+		if L[i] <= R[j]
+			arr[k] = L[i];
+			i <- i + 1;		
+		else 
+			arr[k] = R[j];
+			j <- j + 1;
+		k <- k + 1;	
+	
+	while i < n1 do
+		arr[k] <- L[i];
+		i <- i + 1;
+		k <- k + 1;
+		
+	while j < n2) 
+		arr[k] <- R[j];
+		j <- j + 1;
+		k <- k + 1;
+```
